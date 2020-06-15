ce260e389fd336ab225cd1863f642b08f1b958ee
JongilSohn <64876056+JongilSohn@users.noreply.github.com>
2020-06-15 18:54:36 +0900

    Update

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sonjong-il/Desktop/Project_Pack/venv"
+set -gx VIRTUAL_ENV "/Users/sonjong-il/Desktop/Project_Collections/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
